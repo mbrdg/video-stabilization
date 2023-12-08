2e7386bae3dcf5366a2326bde89dfd5d6e389c75
Gui <guilherme.gomes.franco@gmail.com>
2023-12-08 19:21:00 +0100

    Ãdded motion smoothing method

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import smoothing
 import torchvision.transforms.functional as F
 from torchvision.io import read_video
 from torchvision.models.optical_flow import raft_small, Raft_Small_Weights
@@ -15,6 +16,7 @@ from torchvision.utils import flow_to_image
 plt.rcParams["savefig.bbox"] = "tight"
 weights = Raft_Small_Weights.DEFAULT
 transforms = weights.transforms()
+SMOOTHING_RADIUS = 5
 
 
 def parse_arguments():
@@ -23,18 +25,10 @@ def parse_arguments():
         description="Optical flow computation using the RAFT model from PyTorch"
     )
 
-    parser.add_argument(
-        "--file",
-        required=True,
-        type=str,
-        help="input video file"
-    )
+    parser.add_argument("--file", required=True, type=str, help="input video file")
 
     parser.add_argument(
-        "--frames",
-        type=int,
-        default=-1,
-        help="number of frames to be processed"
+        "--frames", type=int, default=-1, help="number of frames to be processed"
     )
 
     parser.add_argument(
@@ -42,14 +36,14 @@ def parse_arguments():
         "--width",
         type=int,
         default=520,
-        help="width of the output frames (must be divisible by 8)"
+        help="width of the output frames (must be divisible by 8)",
     )
     parser.add_argument(
         "-H",
         "--height",
         type=int,
         default=960,
-        help="height of the output frames (must be divisible by 8)"
+        help="height of the output frames (must be divisible by 8)",
     )
 
     return parser.parse_args()
@@ -68,9 +62,7 @@ def main():
     # Read and preprocess the video batches (pairs of consecutive frames)
     frames, _, _ = read_video(args.file, output_format="TCHW", pts_unit="sec")
     first_batch, follow_batch = preprocess(
-        frames[0:args.frames],
-        frames[1:args.frames + 1],
-        (args.width, args.height)
+        frames[0 : args.frames], frames[1 : args.frames + 1], (args.width, args.height)
     )
     print(f"shape = {first_batch.shape}, dtype = {first_batch.dtype}")
 
@@ -105,11 +97,23 @@ def main():
         ax[1].imshow(np.asarray(flow_img))
         return ax
 
+    def motion_compensation(transformations):
+        # Compute trajectory using cumulative sum of transformations
+        trajectory = np.cumsum(transformations, axis=0)
+
+        # Smooth trajectory using moving average filter
+        smoothed_trajectory = smoothing.smooth(trajectory, SMOOTHING_RADIUS)
+
+        # Calculate difference in smoothed_trajectory and trajectory
+        difference = smoothed_trajectory - trajectory
+
+        # Calculate newer transformation array
+        transforms_smooth = transformations + difference
+
+        return transforms_smooth
+
     animation.FuncAnimation(
-        fig, 
-        animate_callback,
-        repeat=False, 
-        frames=flow_imgs.shape[0]
+        fig, animate_callback, repeat=False, frames=flow_imgs.shape[0]
     )
     plt.tight_layout()
     plt.show()
